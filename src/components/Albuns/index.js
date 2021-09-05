afce34df74425e90408546be848f45ff22f930a1
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-09-05 01:31:09 -0300

    tentando fazer input de busca

--- src/components/Albuns/index.js
+++ src/components/Albuns/index.js
@@ -5,31 +5,79 @@ import '../global.css';
 
 const Albuns = () => {
    const [albuns, setAlbuns] = useState([]);
+   const [filterAlbuns, setFilterAlbuns] = useState([])
+
    useEffect(() => {
-      axios.get("https://jsonplaceholder.typicode.com/albums?_limit=20")
+      axios.get("https://jsonplaceholder.typicode.com/albums?_limit=6")
          .then((response) => {
             setAlbuns(response.data)
+            setFilterAlbuns([])
          })
          .catch((err) => {
             console.log(err)
          })
       // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [])
+   }, []);
 
+   const hadleChange = ({ target }) => {
+      if (!target.value || target.value === "") {
+         setFilterAlbuns([])
+         setAlbuns(albuns)
+         return;
+      }
+      const filterItens = albuns.filter((album) => album.title.startsWith(target.value))
+      if (filterItens) {
+         setFilterAlbuns(filterItens)
+         setAlbuns([])
+      } else {
+         setFilterAlbuns([])
+         setAlbuns(albuns)
+         return;
+      }
+   }
 
    return (
       <section className="itensContainer">
-         <div className="boxContainer">
-            {albuns.map((album) => {
-               return (
-                  <div className="item">
-                     <div className="card">
-                        <p className="itemDescription">{album.title}</p>
-                     </div>
-                  </div>
-               )
-            })}
+         <div className="buscaContainer">
+            <input
+               onChange={hadleChange}
+               className="busca"
+               type="text"
+            />
+            <button className="btnBusca">Buscar</button>
          </div>
+         <div className="boxContainer">
+
+            {
+               albuns.map((album) => {
+
+                  return (
+                     <div className="item">
+                        <div className="card">
+                           <p key={album.id} className="itemDescription">{album.title}</p>
+                        </div>
+                     </div>
+                  )
+               })
+            }
+         </div>
+         <div className="boxContainer">
+            {
+               filterAlbuns.map((album) => {
+
+                  return (
+                     <div className="item">
+                        <div className="card">
+                           <p key={album.id} className="itemDescription">{album.title}</p>
+                        </div>
+                     </div>
+                  )
+               })
+
+            }
+
+         </div>
+
       </section >
    );
 }
