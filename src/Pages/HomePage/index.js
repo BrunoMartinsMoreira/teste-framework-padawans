415bf70b97411cb1f5e4565750e5e03c6895905d
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-09-04 21:49:40 -0300

    pagina home e animações

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from '../../components/HomeComponent';
 
 const HomePage = () => {
    return (
-      <main className="page">
-
+      <main className="homePage">
+         <Home />
       </main>
    );
 }
