c673c7eb298905fd4162806db41a375b24067e38
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-09-05 13:35:49 -0300

    adicionando animações

--- src/components/Albuns/index.js
+++ src/components/Albuns/index.js
@@ -5,13 +5,12 @@ import '../global.css';
 
 const Albuns = () => {
    const [albuns, setAlbuns] = useState([]);
-   const [filterAlbuns, setFilterAlbuns] = useState([])
+
 
    useEffect(() => {
-      axios.get("https://jsonplaceholder.typicode.com/albums?_limit=6")
+      axios.get("https://jsonplaceholder.typicode.com/albums?_limit=20")
          .then((response) => {
             setAlbuns(response.data)
-            setFilterAlbuns([])
          })
          .catch((err) => {
             console.log(err)
@@ -19,32 +18,9 @@ const Albuns = () => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
 
-   const hadleChange = ({ target }) => {
-      if (!target.value || target.value === "") {
-         setFilterAlbuns([])
-         setAlbuns(albuns)
-         return;
-      }
-      const filterItens = albuns.filter((album) => album.title.startsWith(target.value))
-      if (filterItens) {
-         setFilterAlbuns(filterItens)
-         setAlbuns([])
-      } else {
-         setFilterAlbuns([])
-         setAlbuns(albuns)
-         return;
-      }
-   }
-
    return (
       <section className="itensContainer">
          <div className="buscaContainer">
-            <input
-               onChange={hadleChange}
-               className="busca"
-               type="text"
-            />
-            <button className="btnBusca">Buscar</button>
          </div>
          <div className="boxContainer">
 
@@ -61,23 +37,6 @@ const Albuns = () => {
                })
             }
          </div>
-         <div className="boxContainer">
-            {
-               filterAlbuns.map((album) => {
-
-                  return (
-                     <div className="item">
-                        <div className="card">
-                           <p key={album.id} className="itemDescription">{album.title}</p>
-                        </div>
-                     </div>
-                  )
-               })
-
-            }
-
-         </div>
-
       </section >
    );
 }
